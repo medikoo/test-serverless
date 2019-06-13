c631de938981aa60aa3122fe46fcf2eaf6deb5a1
Mariusz Nowak <mariusz@medikoo.com>
2019-06-13 16:18:13 +0200

    Update

--- .serverless/myFunction/index.js
+++ .serverless/myFunction/index.js
@@ -1,14 +1,15 @@
 "use strict";
 
-module.exports.handler = (event, context, callback) =>
+module.exports.handler = (event, context, callback) => {
 	callback(null, {
 		statusCode: 200,
 		body: JSON.stringify(
 			{
-				message: "Go Serverless v1.0! Your function executed successfully (again)!",
+				message: "Regular lambda test",
 				input: event
 			},
 			null,
 			2
 		)
 	});
+};
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports.handler = (event, context, callback) => {
 		statusCode: 200,
 		body: JSON.stringify(
 			{
-				message: "Go Serverless v1.0! Your function executed successfully (again)!",
+				message: "Regular lambda test",
 				input: event
 			},
 			null,
