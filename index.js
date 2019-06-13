24032361f74b7abef45616e9eff3728259264641
Mariusz Nowak <mariusz@medikoo.com>
2019-06-13 12:26:40 +0200

    Update handler

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 "use strict";
 
-module.exports.handler = (event, context, callback) =>
+module.exports.handler = (event, context, callback) => {
+	console.log("ORIGINAL HANDLER");
 	callback(null, {
 		statusCode: 200,
 		body: JSON.stringify(
 			{
-				message: "Go Serverless v1.0! Your function executed successfully (again)!",
-				input: event
+				message: "Go Serverless v1.0! Your function executed successfully (again2)!",
+				input: event,
+				env: process.env
 			},
 			null,
 			2
 		)
 	});
+};
