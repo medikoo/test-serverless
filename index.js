3476967361279e70ab33a77c6ba13d9c36a66c95
Mariusz Nowak <mariusz@medikoo.com>
2019-06-13 15:24:39 +0200

    Configure timeout

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 "use strict";
 
 module.exports.handler = (event, context, callback) => {
-	callback(null, {
-		statusCode: 200,
-		body: JSON.stringify(
-			{
-				message: "Go Serverless v1.0! Your function executed successfully (again)!",
-				input: event
-			},
-			null,
-			2
-		)
-	});
+	setTimeout(
+		() =>
+			callback(null, {
+				statusCode: 200,
+				body: JSON.stringify(
+					{
+						message: "Go Serverless v1.0! Your function executed successfully (again)!",
+						input: event
+					},
+					null,
+					2
+				)
+			}),
+		10000
+	);
 };
