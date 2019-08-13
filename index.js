fc5bf84b288f207f1c0eb4fae971716e722d29d9
Mariusz Nowak <mariusz@medikoo.com>
2019-08-13 14:34:08 +0200

    Update

--- index.js
+++ index.js
@@ -1,19 +1,92 @@
 "use strict";
 
 const ApiGateway = require("aws-sdk/clients/apigateway");
+const Iam = require("aws-sdk/clients/iam");
+const wait = require("timers-ext/promise/sleep");
 
-module.exports.handler = (event, context, callback) => {
+const apiGatewayPushToCloudWatchLogsPolicyArn =
+	"arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs";
+const defaultRoleArn = "arn:aws:iam::992311060759:role/serverlessApiGatewayCloudWatchRole";
+
+module.exports.handler = async () => {
 	const apiGateway = new ApiGateway();
-	apiGateway
-		.updateAccount({
-			patchOperations: [
-				{
-					op: "replace",
-					path: "/cloudwatchRoleArn",
-					value: "arn:aws:iam::992311060759:role/api-gateway-cloudwatch"
-				}
-			]
-		})
-		.promise()
-		.then(result => callback(null, result), error => callback(null, { error }));
+
+	const assignedRoleArn = (await apiGateway.getAccount().promise()).cloudwatchRoleArn;
+	const roleArn = assignedRoleArn || defaultRoleArn;
+	const roleName = roleArn.slice(roleArn.lastIndexOf("/") + 1);
+
+	const iam = new Iam();
+
+	const attachedPolicies = await (async () => {
+		try {
+			return (await iam.listAttachedRolePolicies({ RoleName: roleName }).promise())
+				.AttachedPolicies;
+		} catch (error) {
+			if (error.code === "NoSuchEntity") {
+				// Role doesn't exist yet, create;
+				await iam
+					.createRole({
+						AssumeRolePolicyDocument: JSON.stringify({
+							Version: "2012-10-17",
+							Statement: [
+								{
+									Effect: "Allow",
+									Principal: {
+										Service: ["apigateway.amazonaws.com"]
+									},
+									Action: ["sts:AssumeRole"]
+								}
+							]
+						}),
+						Path: "/",
+						RoleName: roleName
+					})
+					.promise();
+				return [];
+			}
+			throw error;
+		}
+	})();
+
+	if (
+		!attachedPolicies.some(
+			policy => policy.PolicyArn === apiGatewayPushToCloudWatchLogsPolicyArn
+		)
+	) {
+		await iam
+			.attachRolePolicy({
+				PolicyArn: apiGatewayPushToCloudWatchLogsPolicyArn,
+				RoleName: roleName
+			})
+			.promise();
+	}
+
+	if (assignedRoleArn === roleArn) return null;
+
+	const codes = [];
+	const updateAccount = async (counter = 1) => {
+		try {
+			const result = await apiGateway
+				.updateAccount({
+					patchOperations: [
+						{
+							op: "replace",
+							path: "/cloudwatchRoleArn",
+							value: roleArn
+						}
+					]
+				})
+				.promise();
+			return { codes, result };
+		} catch (error) {
+			if (counter < 10) {
+				await wait(10000);
+				codes.push(error.code);
+				return updateAccount(++counter);
+			}
+			return { command: "updateAccount", counter, error };
+		}
+	};
+
+	return updateAccount();
 };
