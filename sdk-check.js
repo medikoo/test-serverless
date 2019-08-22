50ccc5cfd0ff278cf6e8dd80e2ad9c30ce26733b
Mariusz Nowak <mariusz@medikoo.com>
2019-08-22 16:40:59 +0200

    Add check

--- sdk-check.js
+++ sdk-check.js
@@ -1,1 +1,14 @@
 "use strict";
+
+process.env.AWS_REGION = "us-east-1";
+
+const Cloudformation = require("aws-sdk/clients/cloudformation");
+
+const cloudformation = new Cloudformation();
+
+cloudformation
+	.listExports({})
+	.promise()
+	.then(result => {
+		console.log(result);
+	});
