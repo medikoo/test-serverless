a932f4a30b2d6e9d214b4047b97e31403890a9cd
Mariusz Nowak <mariusz@medikoo.com>
2019-08-23 10:06:17 +0200

    Update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports.handler = (event, context, callback) =>
 		statusCode: 200,
 		body: JSON.stringify(
 			{
-				message: "Go Serverless v1.0! Your function executed successfully (again)!",
+				message: "Go Serverless v1.0! Your function executed successfully!",
 				input: event
 			},
 			null,
