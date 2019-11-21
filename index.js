ab5509fbae1ea84a2d9b4e1b370e388ecd387e24
Mariusz Nowak <mariusz@medikoo.com>
2019-11-21 12:16:21 +0100

    Update

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 "use strict";
 
-let lastInvocationId = 0;
-let currentInvocationId;
-
-module.exports.handler = (event, context, callback) => {
-	const invocationId = (currentInvocationId = ++lastInvocationId);
-	console.log("Start invocation", invocationId, Date.now());
-	const currentTime = Date.now();
-	setTimeout(() => {
-		console.log(
-			`Registered for invocation: ${ invocationId } but invoked in invocation: ${
-				currentInvocationId
-			} after ${ Date.now() - currentTime }ms at ${ Date.now() }`
-		);
-	}, 15000).unref();
-	setTimeout(() => callback(null, {}), 3000);
-};
+module.exports.handler = (event, context, callback) =>
+	new Promise(resolve =>
+		setTimeout(() => {
+			// Commenting below line will invoke context.callbackWaitsForEmptyEventLoop = false mode
+			// Which means console.log will not be invoked in this invocation but in first further that occurs after 5s
+			callback(null, {});
+			resolve({});
+			setTimeout(() => console.log("Hello I'm still here!"), 5000);
+		})
+	);
