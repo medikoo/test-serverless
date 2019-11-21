733b8cc2e02f80e5919fc7c36e8030541e5439f4
Mariusz Nowak <mariusz@medikoo.com>
2019-11-21 11:01:14 +0100

    Update

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 "use strict";
 
+let lastInvocationId = 0;
+let currentInvocationId;
+
 module.exports.handler = (event, context, callback) => {
-	callback(null, {
-		statusCode: 200,
-		body: JSON.stringify(
-			{
-				message: "Regular lambda test",
-				input: event
-			},
-			null,
-			2
-		)
-	});
+	const invocationId = (currentInvocationId = ++lastInvocationId);
+	console.log("Start invocation", invocationId, Date.now());
+	context.callbackWaitsForEmptyEventLoop = false;
+	const currentTime = Date.now();
+	setTimeout(() => {
+		console.log(
+			`Registered for invocation: ${ invocationId } but invoked in invocation: ${
+				currentInvocationId
+			} after ${ Date.now() - currentTime }ms at ${ Date.now() }`
+		);
+	}, 15000);
+	setTimeout(() => callback(null, {}), 3000);
 };
