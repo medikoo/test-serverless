d817af029c8896abe41e94822983fe920f7eef15
Mariusz Nowak <mariusz@medikoo.com>
2019-11-21 12:22:53 +0100

    Update

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 module.exports.handler = (event, context, callback) =>
 	new Promise(resolve =>
 		setTimeout(() => {
-			// Commenting below line will invoke context.callbackWaitsForEmptyEventLoop = false mode
-			// Which means console.log will not be invoked in this invocation but in first further that occurs after 5s
+			// Commenting below line will imply
+			// `context.callbackWaitsForEmptyEventLoop = false` mode
+			// Which means that below console.log will not be invoked in this invocation
+			// but in first future one that occurs after 5s
 			callback(null, {});
 			resolve({});
 			setTimeout(() => console.log("Hello I'm still here!"), 5000);
