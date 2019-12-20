7d9bab8c0c3fd6d22cedefc3a776b04cf12f90ac
Mariusz Nowak <mariusz@medikoo.com>
2019-12-20 13:52:28 +0100

    Update

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 module.exports.handler = (event, context, callback) => {
 	callback(null, {
 		statusCode: 200,
-		body: JSON.stringify({ message: "Provisioned Lambda v2", input: event }, null, 2)
+		body: JSON.stringify({ message: "Provisioned Lambda v3", input: event }, null, 2)
 	});
 };
