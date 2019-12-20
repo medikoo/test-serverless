b1b82bc91a8353eb084fb2dc926f090f2f1526c7
Mariusz Nowak <mariusz@medikoo.com>
2019-12-20 17:05:15 +0100

    Update

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 "use strict";
 
-module.exports.handler = (event, context, callback) =>
+const startedAt = Date.now();
+
+module.exports.handler = (event, context, callback) => {
+	console.log("I'm in", Date.now() - startedAt);
 	callback(null, {
 		statusCode: 200,
-		body: JSON.stringify({ message: "Lambda test v2" }, null, 2)
+		body: JSON.stringify({ message: "Lambda test v4" }, null, 2)
 	});
+};
