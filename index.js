cf2f738f17ef447dab5080c25fd938473c24bf2c
Mariusz Nowak <mariusz@medikoo.com>
2019-12-23 09:50:40 +0100

    Update

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 const startedAt = Date.now();
 
 module.exports.handler = (event, context, callback) => {
-	console.log("I'm in", Date.now() - startedAt);
+	console.log("I'm in", Date.now() - startedAt, event);
 	callback(null, {
 		statusCode: 200,
-		body: JSON.stringify({ message: "Lambda test v4" }, null, 2)
+		body: JSON.stringify({ message: "Lambda test v5" }, null, 2)
 	});
 };
