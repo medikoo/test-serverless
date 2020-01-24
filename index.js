3366e9c038c45e63fc182e4a9f4bc1abd7eca883
Mariusz Nowak <mariusz@medikoo.com>
2020-01-24 16:11:18 +0100

    Update

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 "use strict";
 
-module.exports.handler = (event, context, callback) =>
+module.exports.handler = (event, context, callback) => {
+	console.log("INPUT EVENT", event);
 	callback(null, {
 		statusCode: 200,
-		body: JSON.stringify(
-			{
-				message: "Go Serverless v1.0! Your function executed successfully (again)!",
-				input: event
-			},
-			null,
-			2
-		)
+		body: JSON.stringify({ message: "HTTP API TEST", event }, null, 2)
 	});
+};
