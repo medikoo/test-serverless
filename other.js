7191035506edd11471ac537be4e017634eeda2f1
Mariusz Nowak <mariusz@medikoo.com>
2020-01-31 10:28:46 +0400

    Update

--- other.js
+++ other.js
@@ -4,6 +4,6 @@ module.exports.handler = (event, context, callback) => {
 	console.log("INPUT EVENT", event);
 	callback(null, {
 		statusCode: 200,
-		body: JSON.stringify({ message: "HTTP API TEST MAIN", event }, null, 2)
+		body: JSON.stringify({ message: "HTTP API TEST OTHER", event }, null, 2)
 	});
 };
