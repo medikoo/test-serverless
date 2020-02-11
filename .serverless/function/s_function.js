ce5250ca63bb66583fc05f08a6f7cd8bf2a8f1fc
Mariusz Nowak <mariusz@medikoo.com>
2020-02-11 15:07:15 +1300

    Update

--- .serverless/function/s_function.js
+++ .serverless/function/s_function.js
@@ -4,7 +4,7 @@ orgId: 'medikoo',
 applicationName: 'medikoo-test',
 appUid: 'GjXh77HjWzffv8X5SQ',
 orgUid: '1sJrygBdcH5mNKPSp8',
-deploymentUid: 'bc26ba49-0a01-4ebc-8916-581d530a3dd6',
+deploymentUid: 'ece29c50-828c-4eae-8f3b-b4f96a6b9ebc',
 serviceName: 'test-dashboard',
 stageName: 'many-params',
 pluginVersion: '3.3.0'})
