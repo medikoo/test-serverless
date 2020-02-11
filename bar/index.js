489b285381bd83029e07906bc101bfc7d6ca63dd
Mariusz Nowak <mariusz@medikoo.com>
2020-02-12 12:42:08 +1300

    Update

---
+++ bar/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports.handler = (event, context, callback) => {
+	callback(null, {
+		statusCode: 200,
+		body: JSON.stringify({ message: "Regular lambda test", input: event }, null, 2)
+	});
+};
