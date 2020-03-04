92756b81ec8fd062b3034019bf923fa51d24416b
Mariusz Nowak <mariusz@medikoo.com>
2020-03-04 16:09:15 +1300

    Update

--- target.js
+++ target.js
@@ -1,9 +1,9 @@
 "use strict";
 
 module.exports.handler = (event, context, callback) => {
-	console.log("Trigger", event);
+	console.log("Target", event);
 	callback(null, {
 		statusCode: 200,
-		body: JSON.stringify({ message: "Trigger", input: event }, null, 2)
+		body: JSON.stringify({ message: "Target", input: event }, null, 2)
 	});
 };
