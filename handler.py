5b4dbc1dbb527061f4896369c4871000998914c1
Mariusz Nowak <mariusz@medikoo.com>
2020-03-16 13:16:01 +1300

    Update

--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "Go Serverless v1.0! Your Python function executed successfully!",
         "input": event
     }
 
@@ -18,7 +18,7 @@ def hello(event, context):
     # integration
     """
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "Go Serverless v1.0! Your Python function executed successfully!",
         "event": event
     }
     """
