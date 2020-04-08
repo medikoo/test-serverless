73be13bb93c8cc62442cd5b7b2d8669f6f68336a
Mariusz Nowak <mariusz@medikoo.com>
2020-04-08 16:35:26 +1200

    Update

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 module.exports.handler = (event, context, callback) =>
 	callback(null, {
 		statusCode: 200,
-		body: JSON.stringify({ path: event.path, method: event.httpMethod })
+		body: JSON.stringify({ path: event.path, method: event.httpMethod, version: 3 })
 	});
