a99d4d5c4a10bfb99fdf93ead07bd62874565471
Mariusz Nowak <mariusz@medikoo.com>
2020-05-07 10:31:33 +0200

    Update

--- .serverless/function/s_function.js
+++ .serverless/function/s_function.js
@@ -1,17 +1,29 @@
-var serverlessSDK = require('./serverless_sdk/index.js')
+
+var serverlessSDK = require('./serverless_sdk/index.js');
 serverlessSDK = new serverlessSDK({
-tenantId: 'medikoo',
-applicationName: 'medikoo-test',
-appUid: 'GjXh77HjWzffv8X5SQ',
-tenantUid: '1sJrygBdcH5mNKPSp8',
-deploymentUid: '11f3be58-3497-443c-967a-4a356e800e9c',
-serviceName: 'test-dashboard-api-gateway',
-stageName: 'dev',
-pluginVersion: '3.2.6'})
-const handlerWrapperArgs = { functionName: 'test-dashboard-api-gateway-dev-function', timeout: 6}
+  orgId: 'medikoo',
+  applicationName: 'medikoo-test',
+  appUid: 'GjXh77HjWzffv8X5SQ',
+  orgUid: '1sJrygBdcH5mNKPSp8',
+  deploymentUid: '6e510c02-a3ed-4971-aa13-619b85d57cb0',
+  serviceName: 'test-dashboard-api-gateway',
+  shouldLogMeta: true,
+  shouldCompressLogs: true,
+  disableAwsSpans: false,
+  disableHttpSpans: false,
+  stageName: 'dev',
+  serverlessPlatformStage: 'prod',
+  devModeEnabled: false,
+  accessKey: null,
+  pluginVersion: '3.6.10',
+  disableFrameworksInstrumentation: false
+});
+
+const handlerWrapperArgs = { functionName: 'test-dashboard-api-gateway-dev-function', timeout: 6 };
+
 try {
-  const userHandler = require('./index.js')
-  module.exports.handler = serverlessSDK.handler(userHandler.handler, handlerWrapperArgs)
+  const userHandler = require('./index.js');
+  module.exports.handler = serverlessSDK.handler(userHandler.handler, handlerWrapperArgs);
 } catch (error) {
-  module.exports.handler = serverlessSDK.handler(() => { throw error }, handlerWrapperArgs)
+  module.exports.handler = serverlessSDK.handler(() => { throw error }, handlerWrapperArgs);
 }
