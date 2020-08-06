0b83762b0603f13780b93ea78b6485194b89cf7b
Mariusz Nowak <medyk@medikoo.com>
2020-08-06 15:20:55 +0200

    Update

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ module.exports.handler = (event, context, callback) => {
 	console.log("Event", event);
 	callback(null, {
 		statusCode: 200,
-		body: JSON.stringify({ message: "Success!", input: event }, null, 2)
+		body: JSON.stringify({ message: "Success2!", input: event }, null, 2)
 	});
 };
