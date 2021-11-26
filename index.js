3d6c4a650a51538f174a42263eb2b9e3a650e8d6
Mariusz Nowak <medyk@medikoo.com>
2021-11-26 17:17:15 +0100

    Update

--- index.js
+++ index.js
@@ -3,8 +3,5 @@
 const fsp  = require("fs").promises
     , path = require("path");
 
-module.exports.handler = async () => ({
-	statusCode: 200,
-	body: (await fsp.readFile(path.resolve(__dirname, "image.jpg"))).toString("base64"),
-	headers: { "Content-Type": "image/jpg" }
-});
+module.exports.handler = async () =>
+	(await fsp.readFile(path.resolve(__dirname, "image.jpg"))).toString("base64");
