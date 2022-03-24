9d6884281463e43656c0da26d4992e8def7f3fca
Mariusz Nowak <medyk@medikoo.com>
2022-03-24 19:26:20 +0100

    Update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-"use strict";
-
-module.exports.handler = (event, context, callback) => {
-	console.log("Evedt", event);
-	callback(null, {
-		statusCode: 200,
-		body: JSON.stringify({ message: "Regular lambda test", input: event }, null, 2)
-	});
+export const handler = (event, context, callback) => {
+	process._rawDebug("F (ESM)", "invocation");
+	setTimeout(() => {
+		process._rawDebug("F (ESM)", "return");
+		callback(null, {
+			statusCode: 200,
+			body: JSON.stringify({ event, env: process.env, context, argv: process.argv })
+		});
+	}, 2000);
 };
