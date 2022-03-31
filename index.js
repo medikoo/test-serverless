157757929409280af3942c25968e2a307907dbfd
Mariusz Nowak <medyk@medikoo.com>
2022-03-31 21:11:00 +0200

    Update

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 "use strict";
 
-module.exports.handler = (event, context, callback) => {
-	console.log("Evedt", event);
-	callback(null, {
-		statusCode: 200,
-		body: JSON.stringify({ message: "Regular lambda test", input: event }, null, 2)
-	});
-};
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	console.log("test from root");
+	console.log("event ", req.body);
+	return res.status(200).json({ message: "Hello from root!" });
+});
+
+app.get("/hello", (req, res) => {
+	console.log("test from hello");
+	console.log("event ", req.body);
+	return res.status(200).json({ message: "Hello from path!" });
+});
+
+app.post("/testdestination", (req, res) => {
+	console.log(JSON.stringify(req.body));
+	return res.status(200).json({ msg: "hello" });
+});
+
+app.use((req, res) => res.status(404).json({ error: "Not Found" }));
+
+module.exports.handler = serverless(app);
